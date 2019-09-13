fb1f0cdee3420977866a429bbc6ec06e99a3753d
AnhhTuann <tuannguyen25101999@gmail.com>
2019-09-13 22:40:40 +0700

    add css module in react

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,44 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-module.exports = {
-  resolve: {
-    extensions: [".js", ".jsx"]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
+  const HtmlWebPackPlugin = require("html-webpack-plugin");
+  module.exports = {
+    resolve: {
+      extensions: [".js", ".jsx"]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
           }
-        ]
-      }
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: "html-loader"
+            }
+          ]
+        },
+        {
+          test: /\.css$/i,
+          exclude: /\.lazy\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.lazy\.css$/i,
+          use: [
+            { loader: 'style-loader', options: { injectType: 'lazyStyleTag' } },
+            'css-loader',
+          ],
+        },
+        
+      ]
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html"
+      })
     ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
-    })
-  ]
-};  
+  };  
